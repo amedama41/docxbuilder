626c5c2f7d36b22706885ac77198362c02f1d2e9
amedama41 <kamo.tanabota@gmail.com>
2018-08-04 15:59:06 +0900

    Change supported sphinx version because an API's interface has been changed on current sphinx

--- docxbuilder/builder.py
+++ docxbuilder/builder.py
@@ -57,7 +57,7 @@ class DocxBuilder(Builder):
     def assemble_doctree(self):
         master = self.config.master_doc
         tree = self.env.get_doctree(master)
-        tree = inline_all_toctrees(self, set(), master, tree, darkgreen)
+        tree = inline_all_toctrees(self, set(), master, tree, darkgreen, [])
         tree['docname'] = master
         self.env.resolve_references(tree, master, self)
         self.fix_refuris(tree)
