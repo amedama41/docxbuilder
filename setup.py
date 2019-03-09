9698c40f0185ddf60bb3ce6508b051ab94cd167a
amedama41 <kamo.tanabota@gmail.com>
2019-03-09 09:17:17 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from distutils.command import build
+from setuptools import setup
+
+class CustomBuild(build.build, object):
+    def run(self):
+        from create_style_file import create_style_file
+        create_style_file()
+        return super(CustomBuild, self).run()
+
+setup(
+    name='shpinx-docxbuilder',
+    version='0.0.1',
+    description='Sphinx builder extension that generates docx files',
+    license='MIT',
+    keywords='sphinx',
+    packages=[
+        'docxbuilder',
+        'docxbuilder.docx',
+    ],
+    install_requires=[
+        "pillow",
+        "lxml",
+    ],
+    package_data={
+        'docxbuilder.docx': ['style.docx'],
+    },
+    classifiers=[
+        'Framework :: Sphinx :: Extension',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Documentation :: Sphinx',
+    ],
+    cmdclass={
+        'build': CustomBuild,
+    }
+)
