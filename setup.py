d0e59a14812f8b863713d4fd41138f2be0807b1f
amedama41 <kamo.tanabota@gmail.com>
2019-03-24 16:29:34 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from distutils.command import build
 from setuptools import setup
 
@@ -8,12 +9,20 @@ class CustomBuild(build.build, object):
         create_style_file()
         return super(CustomBuild, self).run()
 
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(BASEDIR, 'README.rst'), 'r') as f:
+    long_description = f.read()
+
 setup(
-    name='sphinx-docxbuilder',
-    version='0.0.1',
-    description='Sphinx builder extension that generates docx files',
+    name='docxbuilder',
+    version='1.0.0',
+    description='Sphinx docx builder extension',
+    long_description=long_description,
+    url='https://github.com/amedama41/docxbuilder',
+    author='amedama41',
+    author_email='kamo.devel41@gmail.com',
     license='MIT',
-    keywords='sphinx',
+    keywords=['sphinx', 'extension', 'docx', 'OpenXML'],
     packages=[
         'docxbuilder',
         'docxbuilder.docx',
@@ -24,13 +33,14 @@ setup(
         "pillow",
         "six",
     ],
+    python_requires='>=2.7, >=3.5',
     package_data={
         'docxbuilder.docx': ['style.docx'],
     },
     classifiers=[
         'Framework :: Sphinx :: Extension',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3',
         'Topic :: Documentation :: Sphinx',
     ],
     cmdclass={
