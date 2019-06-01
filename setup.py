4b9787e61b65a014b5fb19d36c7d5ea5b07cd2be
amedama41 <kamo.tanabota@gmail.com>
2019-06-01 20:57:16 +0900

    Version 1.0.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(BASEDIR, 'README.rst'), 'r') as f:
 
 setup(
     name='docxbuilder',
-    version='1.0.1',
+    version='1.0.2',
     description='Sphinx docx builder extension',
     long_description=long_description,
     url='https://github.com/amedama41/docxbuilder',
